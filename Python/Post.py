06ab73e9ddac377590c557bb697f0f8475a710bf
Yoking Wang <chaos1984@163.com>
2018-02-01 13:03:17 +0800

    Update Post.py
    修改部分错误

--- Python/Post.py
+++ Python/Post.py
@@ -3,7 +3,7 @@
 Created on %(date)
 @author:Yujin Wang
 '''
-import numpy as np,pandas as pd,matplotlib.pylab as plt
+import numpy as np,pandas as pd,matplotlib.pyplot as plt
 from matplotlib.text import OffsetFrom
 from funmodule import *
 
@@ -20,7 +20,7 @@ class basic():
 		self.data = data 
 		self.color = color
 
-	def plot(basic):
+	def plot(self):
 		'''This class is used for plotting and output parameters'''
 		def maxmin(self,period=1e10):
 			max_y,min_y = 0,0
@@ -79,35 +79,35 @@ class basic():
 			bbox = bbox_args,
 			arroeprops = arrow_args,
 			horizontalalignment = 'top')
-	class barplot(basic):
-		'''It is used to plot the bar figure for the senstive analysis and design variables history'''
-		def plot(self,lable):
-			plt.figure(num=self.num)
-			plt.subplot(self,figrepos)
-			res_data = self.data.sort_values(by=lable,ascending = False).iloc[:20]
-			plt.bar(range(len(res_data)).tuple(res_data.index),size=20,rotation = 45)
-			plt.yticks(size=20)
-			plt.ylabel(self.ylabel,fontproperties='Simhei',size=20)
-			plt.xlabel(self.xlabel,fontproperties='Simhei',size=20)
-			plt.title(self.title,fontproperties='Simhei',size=20)
-			plt.grid()
+class barplot(basic):
+    '''It is used to plot the bar figure for the senstive analysis and design variables history'''
+    def plot(self,lable):
+        plt.figure(num=self.num)
+        plt.subplot(self,figrepos)
+        res_data = self.data.sort_values(by=lable,ascending = False).iloc[:20]
+        plt.bar(range(len(res_data)).tuple(res_data.index),size=20,rotation = 45)
+        plt.yticks(size=20)
+        plt.ylabel(self.ylabel,fontproperties='Simhei',size=20)
+        plt.xlabel(self.xlabel,fontproperties='Simhei',size=20)
+        plt.title(self.title,fontproperties='Simhei',size=20)
+        plt.grid()
 
-		def opt_plot(nodes,elem_new,res,X_predict,v_predict):
-			Force_Point = list(res.iloc(0))[-1]
-			x_coor = list(res.iloc(1))
-			y_coor = list(res.iloc(2))
-			force_dis = list(res.iloc(4))
-			plt.figure(1)
-			plt.gca().set_aspect('equal')
-			plt.tricontourf(nodes.X,nodes.Y,elem_new,nodes,Z,colors='y')
-			plt.grid()
-			plt.plot(x_coor,y_coor,marker='*')
-			plt.scatter(X_predict[0],X_predict[1],color='r')
-			string = 'NODE: %d\nX: %5.3f	Y:%5.3f		Z:%5.3f\n 	DIS:	%5.4E' %((Force_Point),round(x_coor[-1],2),round(y_coor[-1],2),force_dis[-1])
-			plt.text(x_coor[-1],y_coor[-1],string)
-			plt.figure(2)
-			plt.plot(force_dis)
-			plt.plot(v_predict)
-			plt.grid()
-			plt.show()
-			printtime()
+    def opt_plot(nodes,elem_new,res,X_predict,v_predict):
+        Force_Point = list(res.iloc(0))[-1]
+        x_coor = list(res.iloc(1))
+        y_coor = list(res.iloc(2))
+        force_dis = list(res.iloc(4))
+        plt.figure(1)
+        plt.gca().set_aspect('equal')
+        plt.tricontourf(nodes.X,nodes.Y,elem_new,nodes,Z,colors='y')
+        plt.grid()
+        plt.plot(x_coor,y_coor,marker='*')
+        plt.scatter(X_predict[0],X_predict[1],color='r')
+        string = 'NODE: %d\nX: %5.3f	Y:%5.3f		Z:%5.3f\n 	DIS:	%5.4E' %(Force_Point,round(x_coor[-1],2),round(y_coor[-1],2),round(force_dis[-1]))
+        plt.text(x_coor[-1],y_coor[-1],string)
+        plt.figure(2)
+        plt.plot(force_dis)
+        plt.plot(v_predict)
+        plt.grid()
+        plt.show()
+        printtime()
