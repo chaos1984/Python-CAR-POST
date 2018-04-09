9264b9fc748c764e0a60cc68fa1d8db7e61f011b
Yoking Wang <chaos1984@163.com>
2018-04-09 13:56:44 +0800

    Update pptreport.py

--- Python/pptreport.py
+++ Python/pptreport.py
@@ -3,163 +3,211 @@
 Created on Tue Oct 10 2017
 @author: wangyj04
 '''
-
+import os
 from pptx import Presentation
 from pptx.dml.color import RGBColor
 from pptx.enum.dml import MSO_THEME_COLOR
 from pptx.util import Cm,Pt,Inches
 from pptx.enum.shapes import MSO_SHAPE
-import os
+from pptx.enum.dml import MSO_PATTERN
 
+
+			
 class StiffPPT():
-#载入模板
+#Slid master
 	def __init__(self,muban_path,case_path):
 		self.case_path = case_path
 		self.prs = Presentation(muban_path)
-		#模板库
+		#Layouts
 		self.CoverPage = self.prs.slide_layouts[0]
-		self.ContentPage = self.prs.slide_layouts[1]
-		self.SummaryPage = self.prs.slide_layouts[2]
-		self.BlankPage = self.prs.slide_layouts[3]
-		self.TablePage = self.prs.slide_layouts[4]
+		self.BlankPage = self.prs.slide_layouts[1]
+		self.EndPage = self.prs.slide_layouts[2]
 		self.black = RGBColor(0x00,0x00,0x00)
 		self.white = RGBColor(0xFF,0xFF,0xFF)
+		self.pages = 0
+	
 
-	def CoverPageCreate(self,Title,Company,Date):
+	def CoverPageCreate(self,Title,Date,Author):
 		#CoverPage
-		slide = self.prs.slides.add_slide(self.CoverPage)#增加幻灯片
-		shapes = slide.shapes[0]
-		shapes.text = Title
-		shapes = slide.shapes[1]
-		shapes.text = Company
-		shapes = slide.shapes[2]
-		shapes.text = Date
-
-	def SummaryPageCreate(self,Title = ' ',Analysis = ' ',Software = ' ',Author = ' ',Date = ' ',AnalysisContent = ' ',AnalysisConclusion = ' '):
-		slide = self.prs.slides.add_slide(self.SummaryPage)
-		shapes = slide.shapes[0]
-		shapes.text = Title
-		shapes = slide.shapes[1]
-		shapes.text = ' '
-		shapes = slide.shapes[2]
-		shapes.text = Analysis
+		print '#'*10+'%s' %('COVER PAGE')+'#'*10 
+		slide = self.prs.slides.add_slide(self.CoverPage)
 		shapes = slide.shapes[3]
-		shapes.text = Software
-		shapes = slide.shapes[4]
-		shapes.text = Author
-		shapes = slide.shapes[5]
-		shapes.text = Date
-		shapes = slide.shapes[6]
-		shapes.text = ' '
-		shapes = slide.shapes[7]
-		shapes.text = ' '
-		shapes = slide.shapes[8]
-		shapes.text = ' '
-		shapes = slide.shapes[9]
-		shapes.text = ' '
-		shapes = slide.shapes[10]
-		shapes.text = ' '
-		shapes = slide.shapes[11]
-		shapes.text = ' '
-		shapes = slide.shapes[12]
-		shapes.text = AnalysisContent
-		shapes = slide.shapes[13]
-		shapes.text = AnalysisConclusion
-#目录页
-	def ContentsPageCreate(self,ReportContents):
-		slide = self.prs.slides.add_slide(self.BlankPage)
-		shapes = slide.shapes[0]
-		shapes.text = '目录'
+		shapes.text = Title
 		shapes = slide.shapes[1]
-		shapes.text = ReportContents
+		shapes.text = Author
+		shapes = slide.shapes[2]
+		shapes.text =  Date
+		print '#'*20+'\n'
+	# def SummaryPageCreate(self,Title = ' ',Analysis = ' ',Software = ' ',Author = ' ',Date = ' ',AnalysisContent = ' ',AnalysisConclusion = ' '):
+		# slide = self.prs.slides.add_slide(self.SummaryPage)
+		# shapes = slide.shapes[0]
+		# shapes.text = Title
+		# shapes = slide.shapes[1]
+		# shapes.text = ' '
+		# shapes = slide.shapes[2]
+		# shapes.text = Analysis
+		# shapes = slide.shapes[3]
+		# shapes.text = Software
+		# shapes = slide.shapes[4]
+		# shapes.text = Author
+		# shapes = slide.shapes[5]
+		# shapes.text = Date
+		# shapes = slide.shapes[6]
+		# shapes.text = ' '
+		# shapes = slide.shapes[7]
+		# shapes.text = ' '
+		# shapes = slide.shapes[8]
+		# shapes.text = ' '
+		# shapes = slide.shapes[9]
+		# shapes.text = ' '
+		# shapes = slide.shapes[10]
+		# shapes.text = ' '
+		# shapes = slide.shapes[11]
+		# shapes.text = ' '
+		# shapes = slide.shapes[12]
+		# shapes.text = AnalysisContent
+		# shapes = slide.shapes[13]
+		# shapes.text = AnalysisConclusion
+#Content
 
-#模型描述1
-	def BlankPageCreate(self,haeding,subheading,Paragraphs = [],Pictures = []):
-		slide = self.prs.slides.add_slide(self.ContentPage)
+
+#Description1
+	def BlankPageCreate(self,haeding,subheading,Paragraphs = [],Pictures = [],Movies=[],Tables=[]):
+		self.pages += 1
+		print '#'*10+'PAGE %d' %(self.pages)+'#'*10 
+		slide = self.prs.slides.add_slide(self.BlankPage)
 		shapes = slide.shapes[0]
 		shapes.text = haeding
 		shapes = slide.shapes[1]
 		shapes.text = subheading
 		tf = shapes.text_frame
+
 		try:
-			print (u'本页含有 %d 段文字！' %(len(Paragraphs)))
+			print 'No. Paragraphs:',len(Paragraphs)
 			for i in range(len(Paragraphs)):
 				p = tf.add_paragraph()
 				p.text = Paragraphs[i]
 				p.level = 1
 		except:
-			print (u'文字输入个故事有误！')
+			print (u'Warnning: paragraphs!')
+		
 
+		self.addPictures(slide,Pictures)
+		self.addMovies(slide,Movies)
+		self.addTables(slide,Tables)
+		print '#'*20+'\n'
+		
+	def EndPageCreate(self):
+		self.pages += 1
+		print '#'*10+'PAGE %d' %(self.pages)+'#'*10 
+		slide = self.prs.slides.add_slide(self.EndPage)
+		print '#'*20+'\n'
+
+
+	def addTables(self,slide,Tables):
 		try:
-			print (u'本页含有 %d 图片！' %(len(Pictures)))
-			for i in range(len(Pictures)):
-				slide.shapes.add_picture(self.case_path + Pictures[i][0],Pictures[i][1],Pictures[i][2],Pictures[i][3],Pictures[i][4])
+			print 'No. Tables:',len(Tables)
+			for i in range(len(Tables)):
+				rows = len(Tables[i][0]);cols = len(Tables[i][0][0])
+				MatTable = slide.shapes.add_table(rows,cols,Tables[i][1],Tables[i][2],Tables[i][4],Tables[i][3]).table
+				# MatTable.vert_banding =True
+				MatTable.first_row = False
+				for j in range(rows):
+					
+					for k in range(cols):
+						MatTable.cell(j,k).fill.background()
+						# MatTable.fore_color.rgb = RGBColor(0x01, 0x23, 0x45)
+						MatTable.cell(j,k).text = Tables[i][0][j][k]
+						elem_cell = MatTable.cell(j,k)
+						elem_cell.fill.solid()
+						if j == 0:
+							elem_cell.fill.fore_color.theme_color = MSO_THEME_COLOR.ACCENT_2
+						else:
+							elem_cell.fill.fore_color.theme_color = MSO_THEME_COLOR.ACCENT_3
+					
+						
+						# fill = MatTable.cell(j,k).fill
+						# fill.patterned()
+						# fill.pattern= MSO_PATTERN.PERCENT_90
+						# elem_cell.fill.fore_color.rgb = RGBColor(0x01, 0x23, 0x45)
 		except:
-			print (u'图片输入格式有误！')
-
-#模型描述2
-	def TablePageCreate(self,heading,subheading,TableValue,Pictures=[]):
-		rows = len(TableValue);cols = len(TableValue[0])
-		slide = self.prs.slides.add_slide(self.TablePage)
-		shapes = slide.shapes[0]
-		shapes.text = heading
-		shapes = slide.shapes[1]
-		shapes.text = subheading
-
-		shapes = slide.shapesp[2]
-		MatTable = shapes.insert_table(row,cols).table 
-		MatTable,first_row = False
-		#Set column widths
-		MatTable.columns[0].width = Inches(2.0)
-		#Write column heading
-		for i in range(rows):
-			for j in range(cols):
-				MatTable.cell(i,j).fill.background()
-				MatTable.cell(i,j).text = TableValue[i][j]
-		try:
-			print (u'本页含有 %d 图片！' %(len(Pictures)))
-			for i in range(len(Pictures)):
-				slide.shapes.add_picture(self.case_path + Pictures[i][0],Pictures[i][1],Pictures[i][2],Pictures[i][3],Pictures[i][4])
-		except:
-			print(u'图片输入格式有误！')
-
-	def PPTCreate(self,AnalysisType)：
-		self.prs.save(self.case_path + AnalysisType)
-
-if __name__ == '__main__':
-	Title = u'SC00白车身扭转刚度\分析报告（焊接工艺变更）'
-	Company = u'**汽车有限公司'
-	Analysis = u'强度耐久'
-	Author = u'王予津'
-	Date = '2017.10.17'
-	Software = 'MD_NASTRAN2010'
-	ReportContentsList = ['一.模型描述',' 二.计算结果','三.分析结论']
-	AnalysisContent = '本报告'
-	AnalysisConclusion = 'BIP模型的扭转刚度达到10000'
-	ReportContents = '\n'.join(ReportContentsList)
-	case_path = r'\n'
-	data_path = r'\n'
-	muban_path = r'\n'
+			print (u'Warnning: Tables!')
+			
 	
+	def addPictures(self,slide,Pictures):
+		try:
+			print 'No. Pictures:',len(Pictures)
+			for i in range(len(Pictures)):
+				slide.shapes.add_picture(self.case_path + Pictures[i][0],Pictures[i][1],Pictures[i][2],Pictures[i][4],Pictures[i][3])
+		except:
+			print (u'Warnning: figures!')
+
+	def addMovies(self,slide,Movies):
+		try:
+			print 'No. Movies:',len(Movies)
+			for i in range(len(Movies)):
+				slide.shapes.add_movie(self.case_path + Movies[i][0],Movies[i][1],Movies[i][2],Movies[i][4],Movies[i][3])
+		except:
+			print (u'Warnning: movies!')	
+
+	def PPTCreate(self,AnalysisType):
+		print self.case_path + AnalysisType+'.pptx'
+		self.prs.save(self.case_path + AnalysisType+'.pptx')
+
+
+
+			
+if __name__ == '__main__':
+	AnalysisType ='Anchor'
+	Title = u'ESR-036622_SX11_AND_SX12_rear_single_anchor_plate_strength'
+	Date = '2018-03-09'
+	Author = u'Allen Zhu'
+	ReportContentsList = ['1.FE Model\n','2.Material Specification\n','3.Load & Boundary Condition\n','4.Results\n','5.Conclusion\n']
+	AnalysisContent = 'FE model'
+	AnalysisConclusion = 'Current maximum displacement occurs on the anchor plate is 16.7mm, and no breakage occurs on the anchor plate during a tensile force of 22KN.In general, current design meets the analysis requirement.'
+	ReportContents = '\n'.join(ReportContentsList)
+	case_path = r"C:\Users\yujin.wang\Desktop\New_folder/"
+	data_path = r'\n'
+	muban_path = r'C:\Users\yujin.wang\Desktop\New_folder\ALV_General Presentation 2017.pptx'
+
 
 	A = StiffPPT(muban_path,case_path)
-	#封面
-	A.CoverPageCreate(Title,Company)
-	#目录页
-	A.SummaryPageCreate(Title=Title,Analysis=Analysis,Software=Software,Author=Author,Date=Date,AnalysisContent=AnalysisContent,AnalysisConclusion=AnalysisConclusion)
-	#概要页
-	A.ContentsPageCreate(ReportContents)
-	#模型描述页
-	Paragraphs = [u'12',u'32',u'312']
-	Pictures = ['/car.png',Cm(2.4),Cm(8),Cm(16),Cm(12)]
-	A.BlankPageCreate(ReportContentsList[0],'有限元模型信息',paragraphs,Pictures=Pictures)
-	#材料页
-	TableValue = [[' ','弹性模量\n（MPa）','泊松比','密度\n(Ton/mm^3'],['1','弹性模量\n（MPa）','泊松比','密度\n(Ton/mm^3']]
-	A.TablePageCreate(ReportContentsList[0],'材料模型',TableValue)
-	#分析结果页
-	Pictures = [['/bend.png',Cm(2.4),Cm(8),Cm(16),Cm(12)],['/torq.png',Cm(2.4),Cm(8),Cm(16),Cm(12)]]
-	A.BlankPageCreate(ReportContentsList[1],'扭转刚度',Pictures=Pictures)
-	#总结页
-	paragraphs = ['BIP模型的扭转刚度']
-	A.BlankPageCreate(ReportContentsList[2],'经过计算，结果如下：',paragraphs)
-	A.PPTCreate
+
+	#page1
+	A.CoverPageCreate(Title,Date,Author)
+
+	#page2
+	A.BlankPageCreate('Contents',ReportContents)
+
+	#Page3
+	Pictures = [[r'image\test.png',Cm(1),Cm(10),Cm(8),Cm(15)]]
+	TableValue =[[' ','EX\n(MPa)','PRXY','DENS\n(Ton/mm^3)'],['1','2.1E11','0.3','7850']]
+	Tables = [[TableValue ,Cm(16),Cm(3),Cm(4),Cm(16)]]
+	A.BlankPageCreate('FE Model','',['12','32'],Pictures=Pictures,Tables=Tables)
+
+	#page4
+	Pictures = [[r'image\test.png',Cm(16),Cm(4),Cm(8),Cm(16)],[r'image\test.png',Cm(11),Cm(4),Cm(8),Cm(16)]]
+	A.BlankPageCreate("Material Specification","Material Type: S500MC\nDensity 7.85e-09 t/mm3 \n Young's modulus 210000 Mpa\n New failure model has been used for this material element will be removed if failure occur",Pictures=Pictures)
+
+	#page5
+	Pictures = [[r'image\test.png',Cm(8),Cm(5),Cm(12),Cm(18)]]
+	A.BlankPageCreate('Material Specification',"S500MC\nDensity 7.85e-09 t/mm3 \n Young's modulus 210000 Mpa\n New failure model has been used for this material element will be removed if failure occur",Pictures=Pictures)
+
+	#page6
+	Pictures = [[r'image\test.png',Cm(15),Cm(5),Cm(6),Cm(13)],[r'image\test.png',Cm(15),Cm(12),Cm(6),Cm(13)]]
+	Movies = [[r'image\111.avi',Cm(3),Cm(8),Cm(10),Cm(9)]]
+	A.BlankPageCreate('Simulation Results (load 1)','',Pictures=Pictures,Movies=Movies)
+
+
+	#page7
+	load_list =[ ]
+
+	Pictures = [[r'image\test.png',Cm(15),Cm(5),Cm(6),Cm(13)],[r'image\test.png',Cm(15),Cm(12),Cm(6),Cm(13)]]
+	Movies = [[r'image\111.avi',Cm(3),Cm(8),Cm(10),Cm(9)]]
+	A.BlankPageCreate('Simulation Results (load 2)','',Pictures=Pictures,Movies=Movies)
+
+	#page8
+	A.BlankPageCreate('Conclusion','Current maximum displacement occurs on the anchor plate is 16.7mm, and no breakage occurs on the anchor plate during a tensile force of 22KN.\nIn general, current design meets the analysis requirement.')
+
+	A.EndPageCreate()
+	A.PPTCreate(AnalysisType)
